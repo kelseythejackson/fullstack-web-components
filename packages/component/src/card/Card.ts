0df7c71a14adf74729cf41ab80de2433b259877d
Kelsey Jackson <kelseyjackson@Kelseys-MacBook-Pro-2.local>
2024-11-14 03:13:11 -0600

    page 57 - Styling Content

--- packages/component/src/card/Card.ts
+++ packages/component/src/card/Card.ts
@@ -6,6 +6,16 @@ export class CardComponent extends HTMLElement {
 
     const template = document.createElement('template');
     template.innerHTML = `
+    <style>
+      :host{
+      display: block;
+      background: var(--color-white);
+      border-radius: var(--radius-md);
+      box-shadow: var(--shadow);
+      overflow: hidden;
+      max-width: 320px;
+      }
+    </style>
       <header>
         <slot name="header"></slot>
       </header>
