9058e87221f743d97935694ed518920d0f6598c9
Kelsey Jackson <kelseyjackson@Kelseys-MacBook-Pro-2.local>
2024-11-25 20:57:33 -0600

    started mocking validators for storybook

--- packages/component/src/input/TextInput.stories.js
+++ packages/component/src/input/TextInput.stories.js
@@ -1,14 +1,32 @@
 import {TextInputComponent} from './TextInput';
 import {html} from 'lit-html';
 
-const  PrimaryTemplate = ({}) => html`
-<form>
-    <in-textinput></in-textinput>
-</form>
-`;
+const  PrimaryTemplate = ({}) => {
+    setTimeout(() => {
+        const input = document.querySelector(`[name="username"]`);
+        input.$validator = validators["username"]
+    }, 0);
+    return html`
+        <form>
+            <in-textinput name="username"></in-textinput>
+        </form>`
+};
+
+const validators = {
+    username: {
+        validations: [
+            {
+                flag: {valueMissng: true},
+                message: "Error: Required",
+                condition: (input) => input.required && input.value <= 0,
+            },
+        ],
+    },
+};
 
 export const Primary = PrimaryTemplate.bind({});
 export default {
     title: "Components/Inputs/TextInput",
     component: "in-textinput",
 }
+
