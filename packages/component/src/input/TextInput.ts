0c1f6a0ff4b1acbafb7c4cd2dbc28a38ae240f8f
Kelsey Jackson <kelseythej@gmail.com>
2024-12-03 20:58:10 -0600

    styled inputs

--- packages/component/src/input/TextInput.ts
+++ packages/component/src/input/TextInput.ts
@@ -11,6 +11,54 @@ export class TextInputComponent extends HTMLElement {
     const template = document.createElement('template');
 
     template.innerHTML = `
+      <style>
+        :host {
+          display: block;
+          font-family: var(--font-default);
+          font-size: var(--font-body-sm);
+        }
+        input {
+          height: var(--input-min-dimension);
+          width: 100%;
+          border-radius: var(--radius-sm);
+          border: var(--border-default);
+          font-size: var(--font-body-md);
+          padding-left: var(--padding-sm);
+          outline: none;
+          box-sizing: border-box;
+        }
+        input:focus,
+        input:focus:hover,
+        input:active {
+          border: var(--border-focus);
+        }
+        input.error,
+        input.error:hover,
+        input.error:focus,
+        input.error:active {
+          border: var(--border-error);
+          outline: none;
+          box-shadow: none;
+          color: var(--color-error);
+        }
+        input[disabled] {
+          opacity: var(--color-disable);
+          background: var(--color-disable);
+          border: var(--border-disable);
+        }
+        input[disabled]:hover,
+        input[disabled]:focus,
+        input[disabled]:active {
+          border: var(--border-disable);
+          outline: none;
+          box-shadow: none;
+        }
+        .message {
+          margin-top: var(--margin-xxs);
+          color: var(--color-error);
+          font-weight: var(--font-weight-default);
+        }
+      </style>
             <div class="control">
                 <input type="text" />
             </div>
