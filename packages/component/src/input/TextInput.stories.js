84610fc23b4e1ecc5cb93220e32705a39a6d18fc
Kelsey Jackson <kelseythej@gmail.com>
2024-12-05 02:12:52 -0600

    started form story

--- packages/component/src/input/TextInput.stories.js
+++ packages/component/src/input/TextInput.stories.js
@@ -27,16 +27,72 @@ const ErrorTemplate = ({}) => {
     }, 0);
     return html `<in-textinput type="text" id="username" name="username" required class="form-control"></in-textinput>`
 }
+
+const FormTemplate = ({ headline, onSubmit, onValidate, onFormData}) => {
+    return html`
+        <h4 slot="header">${headline}</h4>
+        <form
+            name="foo"
+            slot="content"
+            @formdata="${onFormData}"
+            @validate="${onValidate}"
+            @submit="${onSubmit}"
+        >
+            <fieldset>
+                <legend>Login Form</legend>
+                <label for="username">Username</label>
+                <in-textinput
+                    type="text"
+                    id="username"
+                    name="name"
+                    required
+                    minlength="8"
+                    class="form-control"
+                ></in-textinput>
+            </fieldset>
+        </form>
+    `
+}
 const validators = {
     username: {
         validations: [
             {
-                flag: {valueMissng: true},
-                message: "Error: Required",
-                condition: (input) => input.required && input.value.length < 2,
+                flag: {valueMissing: true},
+                message: "Error: Required, please enter a username",
+                condition: (input) => input.required && input.value.length <= 0,
             },
+            {
+                flag: { tooShort: true},
+                message: "Error: Minimum length not met, please supply a value with at least 8 characters.",
+                condition: (input) => input.minLength && input.value.length < input.minLength
+            }
         ],
     },
+    password: {
+        validations: [
+            {
+              flag: { valueMissing: true },
+              message: 'Error: Required, please enter a username.',
+              condition: (input) => input.required && input.value.length <= 0,
+            },
+            {
+              flag: { tooShort: true },
+              message:
+                'Error: Minimum length not met, please supply a value with at least 8 characters.',
+              condition: (input) =>
+                input.minLength && input.value.length < input.minLength,
+            },
+            {
+              flag: { patternMismatch: true },
+              message:
+                'Please use at least one uppercase, uppercase letter, special character, and number.',
+              condition: (input) =>
+                input.pattern &&
+                input.value.match(new RegExp(input.pattern)) === null,
+            },
+          ],
+    }
+
 };
 
 export const Primary = PrimaryTemplate.bind({});
@@ -57,6 +113,7 @@ DisabledTemplate.args = {};
 
 export const Error = ErrorTemplate.bind({})
 ErrorTemplate.args = {}
+
 
 
 export default {
