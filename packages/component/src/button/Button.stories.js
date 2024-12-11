e61967adffe89a3cb2ccf1a65a894a7123c7ee6b
Kelsey Jackson <kelseythej@gmail.com>
2024-12-11 08:52:16 -0600

    styling the button component

--- packages/component/src/button/Button.stories.js
+++ packages/component/src/button/Button.stories.js
@@ -1,1 +1,17 @@
 import { ButtonComponent } from './Button';
+import { html } from 'lit-html';
+
+export default {
+  title: 'Components/Inputs/Button',
+  component: 'in-button',
+};
+
+const Template = ({ label, variant }) => html`
+  <button class="${variant}" is="in-button">${label}</button>
+`;
+
+export const Primary = Template.bind({});
+Primary.args = {
+  variant: 'primary',
+  label: 'Button',
+};
