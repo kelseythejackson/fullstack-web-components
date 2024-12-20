96e86f4baa86c181630255d0ab6b88d7d6bf0d87
Kelsey Jackson <kelseythej@gmail.com>
2024-12-19 18:40:55 -0600

    built decorator pattern, Refactor is next

--- packages/common/src/template/shadow.ts
+++ packages/common/src/template/shadow.ts
@@ -1,1 +1,12 @@
-export function attachShadow(context: any, options?: ShadowRootInit) {}
+export function attachShadow(context: any, options?: ShadowRootInit) {
+  const shadowRoot: ShadowRoot = context.attachShadow(
+    options || { mode: 'open' }
+  );
+  const template = document.createElement('template');
+  template.innerHTML = `
+    <style>${context.elementMeta.style}</style>
+    ${context.elementMeta.template}
+    `;
+
+  shadowRoot.appendChild(template.content.cloneNode(true));
+}
