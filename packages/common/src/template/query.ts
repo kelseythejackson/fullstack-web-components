8d5937b256b5c8eedf09aa6321a84a077b7a5cf7
Kelsey Jackson <kelseythej@gmail.com>
2025-01-23 22:25:21 -0600

    created query functions

--- packages/common/src/template/query.ts
+++ packages/common/src/template/query.ts
@@ -43,38 +43,37 @@ export function queryAll(selector: string, root?: HTMLElement): Element[] {
     matches: Element[],
     element?: Element
   ): Element[] {
-    return matches;
-  }
-  if (!element && !matches.length) {
-    matches = matches.concat(Array.from(document.querySelectorAll(selector)));
-  }
-
-  const walk = document.createTreeWalker(
-    element ? element : (document.body as Element),
-    NodeFilter.SHOW_ELEMENT,
-    {
-      acceptNode(node) {
-        return NodeFilter.FILTER_ACCEPT;
+    if (!element && !matches.length) {
+      matches = matches.concat(Array.from(document.querySelectorAll(selector)));
+    }
+    const walk = document.createTreeWalker(
+      element ? element : (document.body as Element),
+      NodeFilter.SHOW_ELEMENT,
+      {
+        acceptNode(node) {
+          return NodeFilter.FILTER_ACCEPT;
+        },
       },
-    },
-    // @ts-ignore
-    false
-  );
-  while (walk.nextNode()) {
-    if ((walk.currentNode as Element).querySelector(selector)) {
-      if (!matches.find((match) => match === walk.currentNode)) {
-        matches = matches.concat(
-          Array.from(walk.currentNode as Element).querySelectoryAll(selector)
-        );
+      // @ts-ignore
+      false
+    );
+    while (walk.nextNode()) {
+      if ((walk.currentNode as Element).querySelector(selector)) {
+        if (!matches.find((match) => match === walk.currentNode)) {
+          matches = matches.concat(
+            Array.from((walk.currentNode as Element).querySelectorAll(selector))
+          );
+        }
+        if ((walk.currentNode as HTMLElement).shadowRoot) {
+          __queryAll(
+            selector,
+            matches,
+            (<unknown>(walk.currentNode as HTMLElement).shadowRoot) as Element
+          );
+        }
       }
     }
-    if ((walk.currentNode as HTMLElement).shadowRoot) {
-      __queryAll(
-        selector,
-        matches,
-        (<unknown>(walk.currentNode as HTMLElement).shadowRoot) as Element
-      );
-    }
+    return matches;
   }
   return __queryAll(selector, [], root ? root : null);
 }
