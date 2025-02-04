b6e88f0cbd04615d8115b705c97841ddb4ab63d4
Kelsey Jackson <kelseythej@gmail.com>
2025-02-04 09:17:00 -0600

    scaffolded class

--- packages/client/src/view/main/MainView.ts
+++ packages/client/src/view/main/MainView.ts
@@ -1,7 +1,21 @@
 import { attachShadow, html, css, Component } from '@in/common';
 
-const styles = css``;
-const shadowTemplate = html``;
+const styles = css`
+  :host {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    height: 100vh;
+  }
+  #content-root {
+    overflow-y: scroll;
+  }
+`;
+const shadowTemplate = html`
+  <header>Header</header>
+  <div id="content-root">Content</div>
+  <footer>Footer</footer>
+`;
 
 @Component({
   selector: 'main-view',
