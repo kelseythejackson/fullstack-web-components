fc8e6a5617ad6f4939b89081c9270f6cf100c3e2
Kelsey Jackson <kelseythej@gmail.com>
2025-02-05 11:55:14 -0600

    created and exported AppHeader

--- packages/client/src/index.ts
+++ packages/client/src/index.ts
@@ -2,3 +2,5 @@ const mainView = document.createElement('main-view');
 document.querySelector('#root').appendChild(mainView);
 
 export { MainView } from './view/main/MainView';
+export { AppHeader } from './component';
+export { ButtonComponent } from '@in/ui';
