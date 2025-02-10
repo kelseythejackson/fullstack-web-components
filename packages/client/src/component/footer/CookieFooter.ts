de27da7cfe5a09bc24ece8b0d7b82c651d489c99
Kelsey Jackson <kelseythej@gmail.com>
2025-02-10 07:28:18 -0600

    added content to the cookie footer

--- packages/client/src/component/footer/CookieFooter.ts
+++ packages/client/src/component/footer/CookieFooter.ts
@@ -1,7 +1,16 @@
 import { attachShadow, html, css, Component } from '@in/common';
 
 const styles = css``;
-const shadowTemplate = html``;
+const shadowTemplate = html`
+  <p class="message">
+    We use cookies to personalize content and ads, to provide social media
+    features and to analyze our traffic.
+  </p>
+  <div class="button-container">
+    <button is="in-button" class="in-button secondary">Deny</button>
+    <button is="in-button" class="in button primary">Allow</button>
+  </div>
+`;
 
 @Component({
   selector: 'cookie-footer',
