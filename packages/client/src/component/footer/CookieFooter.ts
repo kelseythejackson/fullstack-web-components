70dc57abc591be793ade0522616d9c992ba382da
Kelsey Jackson <kelseythej@gmail.com>
2025-02-11 22:28:44 -0600

    styled cookie-footer

--- packages/client/src/component/footer/CookieFooter.ts
+++ packages/client/src/component/footer/CookieFooter.ts
@@ -1,6 +1,30 @@
 import { attachShadow, html, css, Component } from '@in/common';
 
-const styles = css``;
+const styles = css`
+  :host {
+    display: flex;
+    justify-content: space-between;
+    height: 120px;
+    padding-left: var(--margin-lg);
+    padding-right: var(--margin-lg);
+    border-top: var(--border-default);
+    align-items: center;
+  }
+  .button-container {
+    align-self: right;
+    display: flex;
+    height: 40px;
+  }
+  button {
+    margin-left: var(--margin-sm);
+  }
+  @media (max-width: 720px) {
+    :host {
+      flex-direction: column;
+      margin-bottom: var(--margin-lg);
+    }
+  }
+`;
 const shadowTemplate = html`
   <p class="message">
     We use cookies to personalize content and ads, to provide social media
