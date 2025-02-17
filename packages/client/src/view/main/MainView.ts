07483911bfedd9bf37613dc104ceb41c791e5ebd
Kelsey Jackson <kelseythej@gmail.com>
2025-02-17 11:57:58 -0600

    added more to template

--- packages/client/src/view/main/MainView.ts
+++ packages/client/src/view/main/MainView.ts
@@ -15,7 +15,32 @@ const styles = css`
 `;
 const shadowTemplate = html`
   <app-header></app-header>
-  <div id="content-root">Content</div>
+  <div id="content-root">
+    <div class="section">
+      <div class="blurb half right">
+        <h2>Your Last Contact List</h2>
+        <p>
+          Lorem ipsum odor amet, consectetuer adipiscing elit. Orci dis sit
+          himenaeos nullam senectus felis mauris feugiat.
+        </p>
+        <a href="/dashboard" class="cta dashboard-link">View Contacts</a>
+      </div>
+    </div>
+    <div class="section">
+      <div class="blurb">
+        <h2>Turn Group Chats Into Live Events</h2>
+        <p>
+          Lorem ipsum odor amet, consectetuer adipiscing elit. Id fringilla
+          parturient dapibus habitant suspendisse venenatis luctus. Faucibus ad
+          litora mi torquent, mattis placerat congue id ex. Sodales vestibulum
+          facilisi eget eros penatibus laoreet praesent aliquet. Ut vestibulum
+          accumsan molestie, taciti interdum hac tristique. Arcu ante habitasse
+          condimentum ad donec aliquet nisl dolor. Curae magnis amet nam
+          torquent lacinia platea cubilia.
+        </p>
+      </div>
+    </div>
+  </div>
   <cookie-footer></cookie-footer>
 `;
 
