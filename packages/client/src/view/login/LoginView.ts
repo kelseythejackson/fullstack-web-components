d2aa321bfe17396c75a3d8725efb2870a2d27582
Kelsey Jackson <kelseythej@gmail.com>
2025-02-27 08:00:13 -0600

    styling login view page 494

--- packages/client/src/view/login/LoginView.ts
+++ packages/client/src/view/login/LoginView.ts
@@ -1,6 +1,20 @@
 import { attachShadow, css, html, Component } from '@in/common';
 
-const styles = css``;
+const styles = css`
+  :host {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    height: 100vh;
+  }
+  #content-root {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translateX(-50%) translateY(-50%);
+    min-width: 320px;
+  }
+`;
 const shadowTemplate = html`
   <app-header></app-header>
   <div id="content-root">
@@ -9,6 +23,27 @@ const shadowTemplate = html`
       <form name="foo" slot="content">
         <fieldset>
           <legend>Login Form</legend>
+          <label for="username">Username</label>
+          <in-textinput
+            type="text"
+            id="username"
+            name="username"
+            required
+            minlength="5"
+            class="form-control"
+          ></in-textinput>
+          <label for="password">Password</label>
+          <in-textinput
+            type="password"
+            id="password"
+            name="password"
+            required
+            minlength="5"
+            class="form-control"
+          ></in-textinput>
+          <button class="primary submit form-button" is="in-button">
+            Submit
+          </button>
         </fieldset>
       </form>
     </in-card>
