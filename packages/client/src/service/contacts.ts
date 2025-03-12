bdb0ec37166c6eea8b77ed507c9009c99da90cfd
Kelsey Jackson <kelseythej@gmail.com>
2025-03-12 10:34:56 -0500

    creating contact service

---
+++ packages/client/src/service/contacts.ts
@@ -0,0 +1,3 @@
+import { ColumnData } from '@in/ui';
+export type Contact = {};
+export type ContactResponse = {};
--- packages/client/src/view/dashboard/DashboardView.ts
+++ packages/client/src/view/dashboard/DashboardView.ts
@@ -1,10 +1,32 @@
 import { attachShadow, css, html, Component } from '@in/common';
 
-const styles = css``;
+const styles = css`
+  #content-root {
+    display: block;
+    margin: 0;
+    padding: var(--padding-xl);
+    width: 100%;
+  }
+  h1 {
+    font-family: var(--font-default);
+    font-weight: var(--font-weight-default);
+    font-size: var(--font-headline-line-height-md);
+    margin-block-start: 0em;
+    margin-block-end: 0em;
+    margin-top: var(--margin-lg);
+    margin-bottom: calc(var(--margin-lg) * 2);
+  }
+  in-tablecard {
+    display: block;
+    width: calc(100% - var(--padding-xl) * 2);
+  }
+`;
 const shadowTemplate = html`
-  <h1>Dashboard</h1>
-  <a href="/">Landing Page</a>
-  <a href="/login">Login</a>
+  <app-header></app-header>
+  <div id="content-root">
+    <h1>Contacts</h1>
+    <in-tablecard channel="dashboard-channel"></in-tablecard>
+  </div>
 `;
 
 @Component({
